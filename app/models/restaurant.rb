2813c1b154cb4e85a3189402a52bc595cacd64d6
miluska schaeffer <miluska.schaeffer@gmail.com>
2020-11-11 16:29:14 +0100

    restaurant review app

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian), message: "%{value} is not a valid category"}
+
+  has_many :reviews, dependent: :destroy
+  validates_associated :reviews
+end
